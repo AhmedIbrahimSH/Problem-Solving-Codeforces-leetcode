56dce39cff00bea2b0f537f5a55ba08a5a7322ea
Ahmed Ibrahim Sharaf Eldin <89032074+AhmedIbrahimSH@users.noreply.github.com>
2022-05-04 03:06:42 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,45 +1,27 @@
 #include <iostream>
 #include <string>
-#include <cstring>
+#include<algorithm>
 using namespace std;
 
 int main()
 {
 
-
-    string first_string,second_string;
-
-    int sizeofString;
-
-    cin >> first_string;
-    cin >> second_string;
-    sizeofString = first_string.length(); // TO KNOW THE SIZE OF THE STRING
-    char String_1[sizeofString+1],String_2[sizeofString+1]; // TO LATER COMPARE THE CHARACTERS WHATEVER THEIR CASES UPPER / LOWER
-
-    strcpy(String_1, first_string.c_str()); // Copying the 2 string into characters array to be able to use the tolower method
-    strcpy(String_2, second_string.c_str());
-    if(sizeofString != second_string.size())
-        cout << "error"; // checking the length if the string if it more than a 100 characters
-    else{
-        for(int i = 0; i < sizeofString ; i++){
-            if(tolower(String_1[i]) > tolower(String_2[i])){
-                cout << "1";
-                break;
-            }
-            else if(tolower(String_1[i]) < tolower(String_2[i])){
-                cout << "-1";
-                break;
-        }
-            else if(tolower(String_1[i]) == tolower(String_2[i])){
-                if(i == sizeofString-1){
-                   cout <<  "0" ;
-                    break;
-                }
-                else{
-                   continue;
-        }}}
-    }
-
+    string number_As_String , original_string;
+    cin >> number_As_String;
+    int original_number = stoi(number_As_String); // changing from string to integar
+    original_string = number_As_String; // to keep the original value of the string
+    number_As_String.erase(number_As_String.end() -1 );
+    string last_removed = number_As_String;
+    original_string.erase(original_string.end()-2);
+    string beforelast_removed = original_string;
+    int last_number_removed = stoi(last_removed);
+    int beforelast_number_removed = stoi(beforelast_removed);
+    if(original_number > last_number_removed && original_number > beforelast_number_removed)
+        cout << original_number;
+    else if(original_number < last_number_removed && last_number_removed > beforelast_number_removed)
+        cout << last_number_removed;
+    else
+        cout << beforelast_number_removed;
 
     return 0;
 }
